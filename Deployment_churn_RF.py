0f81fd417d2a949dc58b2b91ed84b6638f240a16
SundaramAyyappan <124044345+SundaramAyyappan@users.noreply.github.com>
2023-02-25 09:52:00 +0530

    Update Deployment_churn_RF.py

--- Deployment_churn_RF.py
+++ Deployment_churn_RF.py
@@ -6,7 +6,6 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
